5274f71f2aa759563102a83e899cc0ae1698a520
mhelynne <mariahelynne@gmail.com>
2019-06-11 16:36:53 -0300

    Utilizando locale em vez da função setMap
    O parâmetro locale, na requisão dos dados da API, permite remover a função setMap, pois solicitando os dados com o valor 'pt' do  parâmetro locale as descrições de clima vêm sempre em português.

--- weather.js
+++ weather.js
@@ -4,62 +4,6 @@ var def_city = "Maceio";
 var city_name = "Maceio";
 
 var data;
-var statusMap;
-
-function setMap () {
-	
-	statusMap = new Map();
-
-	statusMap.set("0", "Tempestade forte");
-	statusMap.set("1", "Tempestade tropical");
-	statusMap.set("2", "Furacão");
-	statusMap.set("3", "Tempestades severas");
-	statusMap.set("4", "Tempestades");
-	statusMap.set("5", "Misto de neve e chuva");
-	statusMap.set("6", "Misto chuva e gelo");
-	statusMap.set("7", "Misto neve e gelo");
-	statusMap.set("8", "Geada fina");
-	statusMap.set("9", "Chuviscos");
-	statusMap.set("10", "Congelamento chuva");
-	statusMap.set("11", "Alguns chuviscos");
-	statusMap.set("12", "Alguns chuviscos");
-	statusMap.set("13", "Neve baixa");
-	statusMap.set("14", "Tempestade com neve");
-	statusMap.set("15", "Ventania com neve");
-	statusMap.set("16", "Neve");
-	statusMap.set("17", "Granizo");
-	statusMap.set("18", "Gelo");
-	statusMap.set("19", "Poeira");
-	statusMap.set("20", "Neblina");
-	statusMap.set("21", "Tempestade de areia");
-	statusMap.set("22", "Fumacento");
-	statusMap.set("23", "Vento acentuado");
-	statusMap.set("24", "Ventania");
-	statusMap.set("25", "Tempo frio");
-	statusMap.set("26", "Tempo nublado");
-	statusMap.set("27", "Tempo limpo");
-	statusMap.set("28", "Tempo nublado");
-	statusMap.set("29", "Parcialmente nublado");
-	statusMap.set("30", "Parcialmente nublado");
-	statusMap.set("31", "Tempo limpo");
-	statusMap.set("32", "Ensolarado");
-	statusMap.set("33", "Estrelado");
-	statusMap.set("34", "Ensolarado com muitas nuvens");
-	statusMap.set("35", "Misto chuva e granizo");
-	statusMap.set("36", "Ar quente");
-	statusMap.set("37", "Tempestades isoladas");
-	statusMap.set("38", "Trovoadas dispersas");
-	statusMap.set("39", "Trovoadas dispersas");
-	statusMap.set("40", "Chuvas esparsas");
-	statusMap.set("41", "Pesados neve");
-	statusMap.set("42", "Chuviscos com neve");
-	statusMap.set("43", "Neve pesada");
-	statusMap.set("44", "Sol com poucas nuvens");
-	statusMap.set("45", "Chuva");
-	statusMap.set("46", "Queda de neve");
-	statusMap.set("47", "Tempestades isoladas");
-	statusMap.set("48", "Serviço não disponível");
-}
 
 function requestWeather(city_name) {
 	
@@ -68,7 +12,9 @@ function requestWeather(city_name) {
 	  data: {
 		key: '682b03fb',
 		format: 'json-cors',
-		city_name: city_name
+		city_name: city_name,
+		locale: 'pt'
+
 	  },
 	  success: function( result ) {
 		
@@ -97,7 +43,7 @@ function requestWeather(city_name) {
 			
 			$( "#city" ).html( "Clima em " + city_name );
 			$( "#temp" ).html( "Temperatura atual: " + data.temp +" °C" );
-			$( "#status" ).html( "Descrição: " + statusMap.get(data.condition_code) );
+			$( "#status" ).html( "Descrição: " + data.description );
 			$( "#wind" ).html( "Velocidade do vento: " + data.wind_speedy );
 			$( "#sunrise" ).html( "Nascer do sol: " + data.sunrise );
 			$( "#sunset" ).html( "Por do sol: " + data.sunset );			
@@ -123,7 +69,6 @@ function searchForCity() {
 	}
 }
 
-setMap();
 requestWeather(def_city);
 
 $(document).ready(function() {
