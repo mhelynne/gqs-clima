fd4b79ab0c8f9c7d3b3dad2daea6d1afd17e1f8b
mhelynne <mariahelynne@gmail.com>
2019-06-11 16:27:17 -0300

    Adição da barra de pesquisa
    Na barra de pesquisa é possível digitar o nome de uma cidade e em seguida clicar no botão de pesquisar, ou enter. Caso a cidade exista, os dados serão carregados na página, caso contrário, será emitida uma mensagem de erro.

--- weather.js
+++ weather.js
@@ -1,6 +1,7 @@
 jQuery.support.cors = true;
 
-var def_city = "Vancouver";
+var def_city = "Maceio";
+var city_name = "Maceio";
 
 var data;
 var statusMap;
@@ -58,9 +59,8 @@ function setMap () {
 	statusMap.set("46", "Queda de neve");
 	statusMap.set("47", "Tempestades isoladas");
 	statusMap.set("48", "Serviço não disponível");
-
-	
 }
+
 function requestWeather(city_name) {
 	
 	$.getJSON({
@@ -72,17 +72,42 @@ function requestWeather(city_name) {
 	  },
 	  success: function( result ) {
 		
-		data = result.results;
-		city_name = data.city_name;
-		
-		$( "#city" ).html( "Clima em " + city_name );
-		$( "#temp" ).html( "Temperatura atual: " + data.temp +" °C" );
-		$( "#status" ).html( "Descrição: " + statusMap.get(data.condition_code) );
-		$( "#wind" ).html( "Velocidade do vento: " + data.wind_speedy );
-		$( "#sunrise" ).html( "Nascer do sol: " + data.sunrise );
-		$( "#sunset" ).html( "Por do sol: " + data.sunset );			
-		var url_img = "https://assets.hgbrasil.com/weather/images/"+ data.img_id +".png";
-		$( "#weather-img" ).attr("src",url_img);
+		if(result.by == "default") {
+			
+			$("#city-exists").hide();
+			
+			$( "#city" ).html( "Cidade não localizada" );
+			$( "#temp" ).html( "" );
+			$( "#status" ).html( "" );
+			$( "#wind" ).html( "" );
+			$( "#sunrise" ).html( "" );
+			$( "#sunset" ).html( "" );
+			
+			//var url_img = "https://assets.hgbrasil.com/weather/images/38.png";
+			//$( "#not-img" ).attr("src",url_img);
+			
+			$("#city-not-exists").show();
+			
+		} else {
+			
+			$("#city-not-exists").hide();
+			
+			data = result.results;
+			city_name = data.city_name;
+			
+			$( "#city" ).html( "Clima em " + city_name );
+			$( "#temp" ).html( "Temperatura atual: " + data.temp +" °C" );
+			$( "#status" ).html( "Descrição: " + statusMap.get(data.condition_code) );
+			$( "#wind" ).html( "Velocidade do vento: " + data.wind_speedy );
+			$( "#sunrise" ).html( "Nascer do sol: " + data.sunrise );
+			$( "#sunset" ).html( "Por do sol: " + data.sunset );			
+			var url_img = "https://assets.hgbrasil.com/weather/images/"+ data.img_id +".png";
+			$( "#weather-img" ).attr("src",url_img);
+			
+			$("#city-exists").show();
+			
+			
+		}
 		
 	  }
 	  
@@ -90,6 +115,29 @@ function requestWeather(city_name) {
 	
 }
 
+function searchForCity() {
+	var search_city = $("#search-city").val();
+		
+	if( search_city ) {
+		requestWeather( search_city );		
+	}
+}
+
 setMap();
 requestWeather(def_city);
+
+$(document).ready(function() {
+	
+	$("#search-btn").on("click", function() {		
+		searchForCity();		
+	});
+	
+	$("#search-city").on('keypress',function(e) {
+    if(e.which == 13) {
+        searchForCity();
+    }
+	
+});
+
+});
 
